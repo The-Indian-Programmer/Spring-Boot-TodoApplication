e1bb5b587935d2e1fc011b156691c3cad5ca9780
Sumit Kosta <sumitkosta07@gmail.com>
2026-02-10 20:59:18 +0530

    Established MySQL Database connection using the database logger

---
+++ src/main/java/org/crud/todo/config/DatabaseStartupLogger.java
@@ -0,0 +1,33 @@
+package org.crud.todo.config;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseStartupLogger implements ApplicationRunner {
+    private final DataSource dataSource;
+
+    public DatabaseStartupLogger(DataSource dataSource) {
+        this.dataSource = dataSource;
+        System.out.println("Database connnecttion loaded");
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        System.out.println("Application started with database connection:");
+        try (Connection connection = dataSource.getConnection()) {
+            System.out.println("Database connected successfully");
+            System.out.println("Database URL: " + connection.getMetaData().getURL());
+            System.out.println("Database UserName : " + connection.getMetaData().getUserName());
+        } catch (Exception e) {
+           System.out.println("Database connection failed");
+           e.printStackTrace();
+        }
+    }
+    
+}
